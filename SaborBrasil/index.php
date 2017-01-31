6435c72ed4cb0ead0452d249c5f5aba653c67787
MalcolnSoftEng <malcoln.java@gmail.com>
2017-01-31 10:22:21 -0200

    Add files via upload

---
+++ SaborBrasil/index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Responsive Header </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    
+
+<style type="text/css">
+ {
+  display: inline !important;
+  border: none !important;
+  box-shadow: none !important;
+  height: 1em !important;
+  width: 1em !important;
+  margin: 0 .07em !important;
+  vertical-align: -0.1em !important;
+  background: none !important;
+  padding: 0 !important;
+}
+</style>
+
+<link rel='stylesheet' id='spherasystems-font-css'  href='//fonts.googleapis.com/css?family=PT+Sans%3A300%2C400%2C600%2C700%2C800%2C900%7CTangerine%3A400%2C700&#038;ver=4.7.1' type='text/css' media='all' />
+
+<link rel="stylesheet" href="wordpress/style_malcoln/style.css" type='text/css' media='all'/>
+
+<link rel="stylesheet" href="wordpress/css/theme-responsive.css" type='text/css' media='all'/> <!-- link para .css responsivo-->
+
+<link rel='stylesheet' id='spherasystems-nivoslider-style-css'  href='wordpress/css/nivo-slider.css?ver=4.7.1' type='text/css' media='all' />
+
+
+<script type='text/javascript' src='http://saborbrasilrestaurante.com.br/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://saborbrasilrestaurante.com.br/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='wordpress/js/jquery.nivo.slider.js'></script> <!-- link do JQuery para Slider-->
+<script type='text/javascript' src='wordpress/js/custom.js'></script>
+
+<style>
+        #logo h1 a, 
+        .social_icons h5,
+        .social_icons a,
+        a, 
+        .tm_client strong,
+        #footer a,
+        #footer ul li:hover a, 
+        #footer ul li.current_page_item a,
+        h6,
+        h5,
+        .postmeta a:hover,
+        h1,
+        .sidebar-area ul li a:hover,
+        .blog-post h3.entry-title,
+        .woocommerce ul.products li.product .price{
+          color:#ff0000;
+        }
+        .theme-default .nivo-controlNav a.active, 
+        .slide_more a, 
+        .readmore:hover, .wpcf7 form input[type='submit'], 
+        p.sub input[type='submit'],
+        .cf_button, 
+        #commentform input#submit, 
+        .mobile_nav a, 
+        .pagination ul li span.current, 
+        .pagination ul li:hover a, 
+        .pagination ul li span.current, 
+        .pagination ul li:hover a,
+        form.search-form input[type="submit"]:hover{
+          background-color:#ff0000;
+        }
+        .nivo-caption{}
+        #footer{ background-color:#ff0c0c;}
+</style>
+
+        <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+          <style type="text/css">
+          .header{
+          background: url(http://saborbrasilrestaurante.com.br/wp-content/uploads/2017/01/cropped-CABECALHO_WEB.png) no-repeat;
+          background-position: center top;
+        }
+        #logo h1 a{color:#ffffff}
+          </style>
+        <style type="text/css" id="custom-background-css">
+            body.custom-background { background-color: #fc6423; }
+        </style>
+
+
+  </head>
+    
+<body>
+    
+    <div class="main-wrap">
+      <section id="home_slider"> 
+                   
+                              
+                    <div class="slider-wrapper theme-default"><div id="slider" class="nivoSlider">
+                    <img src="http://saborbrasilrestaurante.com.br/wp-content/uploads/2017/01/fachada3-1.png" alt="Entregamos em Domicílio | Servimos Empresa" title="#slidecaption1" />
+                    <img src="http://saborbrasilrestaurante.com.br/wp-content/uploads/2017/01/delivery2.png" alt="Entregamos em Domicílio | Servimos Empresa" title="#slidecaption2" />
+                    <img src="http://saborbrasilrestaurante.com.br/wp-content/uploads/2017/01/deliverygeral.png" alt="Entregamos em Domicílio | Servimos Empresa" title="#slidecaption3" />                </div>
+
+                      <div id="slidecaption1" class="nivo-html-caption">
+                        <div class="slide_info">
+                                <h1>Entregamos em Domicílio | Servimos Empresa</h1>
+                               <p>Faça seu pedido pela nossa página, é simples e rápido.</p>
+                        </div>                           
+                            <p class="slide_more"><a href="https://www.facebook.com/delivery.saborbrasil/">SAIBA MAIS</a></p>
+                        </div>                    
+                      <div id="slidecaption2" class="nivo-html-caption">
+                        <div class="slide_info">
+                                <h1>Entregamos em Domicílio | Servimos Empresa</h1>
+                               <p>Faça seu pedido pela nossa página, é simples e rápido.</p>
+                        </div>                           
+                            <p class="slide_more"><a href="#2">SAIBA MAIS</a></p>
+                        </div>                    
+                      <div id="slidecaption3" class="nivo-html-caption">
+                        <div class="slide_info">
+                                <h1>Entregamos em Domicílio | Servimos Empresa</h1>
+                               <p>Faça seu pedido pela nossa página, é simples e rápido.</p>
+                        </div>                           
+                            <p class="slide_more"><a href="#3">SAIBA MAIS</a></p>
+                        </div>                  
+                    </div>
+                    <div class="clear"></div>             
+            </section>
+
+
+
+    <div class="wrapper_main" >      
+        <header class="header"> <!--cabeçalho-->
+            <div class="container-header">
+                <div class="head_fix">
+                 <div id="logo">
+                      <h1><a href="http://saborbrasilrestaurante.com.br/" rel="home">SaborBrasil</a></h1>
+                                        <p>RESTAURANTE ROTISSERIE</p>
+                  </div>
+                <div class="header_right">
+              
+                    <div class="social_icons">  
+                        <a href="http://saborbrasilrestaurante.com.br" target="_blank" class="facebook"><img src="images/logo_face_circle.png"></a>
+                        <a href="http://saborbrasilrestaurante.com.br" target="_blank" class="facebook"><img src="images/carrinho"></a>                     
+                                              
+                   </div>  
+                            <h6><a href="mailto:"></a></h6>
+                          <h6>(19) 3891.5453 / 3891.5443 3841.7108</h6>
+                </div>
+                </div><!--end.head_fix-->                                                      
+            </div><div class="clear"></div>   
+            </header>
+
+            <div id="menu_fix">
+             <div class="header_menu">   
+              
+              <div class="mobile_nav"><a href="#">Menu...</a></div>
+                <nav id="nav">
+
+        <div class="menu-meu-pedido-container">
+        <ul >
+        <li >
+        <a href="http://saborbrasilrestaurante.com.br/">INÍCIO</a></li>
+        <li >
+        <a href="http://saborbrasilrestaurante.com.br/?page_id=64">CARDÁPIO DO DIA</a></li>
+        <li >
+        <a href="wpsaborbrasil/testeJavaSLogin.php">FAZER PEDIDO</a></li>
+        <li >
+        <a href="indexSaborBrasilFORPARSE.php">CADASTRE-SE AQUI</a>
+        </li>
+        </ul>
+        </div>              
+                
+              </nav>
+
+             </div> <!--end.header_menu-->  
+             
+             </div>        
+            
+        </div>   
+<div class="mainpage-area">   
+
+
+<div class="clear"></div>         
+</div><!--end .main-wrapper-->
+</div><!--end .main-page-area-->
+<div id="services-area" >
+   <div class="container_services_wrap">
+      <div class="page_wrapper">           
+          <div class="list-services ">
+                <a href="http://saborbrasilrestaurante.com.br/?page_id=2"><img src="http://saborbrasilrestaurante.com.br/wp-content/uploads/2017/01/site_em_construcao-150x150.jpg" alt="" />
+                <h3>Sample Page</h3></a>   
+                <p></p>
+                <a class="readmore" href="http://saborbrasilrestaurante.com.br/?page_id=2">Saiba Mais</a> 
+             </div> 
+          <div class="list-services ">
+                <a href="http://saborbrasilrestaurante.com.br/?page_id=2"><img src="http://saborbrasilrestaurante.com.br/wp-content/uploads/2017/01/site_em_construcao-150x150.jpg" alt="" />
+                <h3>Sample Page</h3></a>   
+                <p></p>
+                <a class="readmore" href="http://saborbrasilrestaurante.com.br/?page_id=2">Saiba Mais</a> 
+             </div> 
+          <div class="list-services ">
+                <a href="http://saborbrasilrestaurante.com.br/?page_id=2"><img src="http://saborbrasilrestaurante.com.br/wp-content/uploads/2017/01/site_em_construcao-150x150.jpg" alt="" />
+                <h3>Sample Page</h3></a>    
+                <p></p>
+                <a class="readmore" href="http://saborbrasilrestaurante.com.br/?page_id=2">Saiba Mais</a> 
+             </div> 
+                                                          
+          <div class="list-services last-cols">
+                <a href="http://saborbrasilrestaurante.com.br/?page_id=2"><img src="http://saborbrasilrestaurante.com.br/wp-content/uploads/2017/01/site_em_construcao-150x150.jpg" alt="" />
+                <h3>Sample Page</h3></a>     
+                <p></p>
+                <a class="readmore" href="http://saborbrasilrestaurante.com.br/?page_id=2">Saiba Mais</a> 
+             </div> 
+                            
+          </div><!--end .page_wrapper-->       
+     </div> <!--end container-->
+   </div><!--end services area-->   
+          <section> 
+          <div class="home-post">
+              <div>
+                   <header class="entry-header">
+                         <h1 class="entry-title">Sabor Brasil Restaurante</h1>
+                   </header><!-- .entry-header -->    
+                    <div class="blog-post">
+                        <div class="blog-post-repeat">
+                            <article id="post-1" class="post-1 post type-post status-publish format-standard hentry category-uncategorized">
+                              <header class="entry-header">
+                                  <h3 class="entry-title"><a href="http://saborbrasilrestaurante.com.br/?p=1" rel="bookmark">Sejam Bem Vindos!</a></h3>
+                                                  <div class="postmeta">
+                                          <div class="post-date">janeiro 1, 2017</div><!-- post-date -->
+                                          <div class="post-comment"> &nbsp;|&nbsp; <a href="http://saborbrasilrestaurante.com.br/?p=1#comments">1 Comentário</a></div>
+                                          <div class="post-categories" style="float:none;"> &nbsp;|&nbsp; <a href="http://saborbrasilrestaurante.com.br/?cat=1" rel="category">Depoimentos:</a></div>
+                                          <div class="clear"></div>
+                                      </div><!-- postmeta -->
+                                              <div class="post-thumb"></div><!-- post-thumb -->
+                              </header><!-- .entry-header -->
+    
+                <div class="entry-summary">
+            <p>Sabor Brasil Restaurante</p>
+            <a class="readmore" href="http://saborbrasilrestaurante.com.br/?p=1">Saiba &rarr;</a>
+        </div><!-- .entry-summary -->
+            
+        <footer class="entry-meta" style="display:none;">
+                                
+                                
+                       <span class="comments-link"><a href="http://saborbrasilrestaurante.com.br/?p=1#comments">1 Comment</a></span>
+                
+                    </footer><!-- .entry-meta -->
+    </article><!-- #post-## -->
+    <div class="clear"></div>
+</div><!-- blog-post-repeat -->                                                                                                                                </div><!-- blog-post -->
+              </div>
+            </div><!-- home-post -->
+        </section><div class="clear"></div>
+    
+
+<footer id="footer">
+  <div class="container-footer">
+    <div class="left">
+               <div class="menu-meu-pedido-container"><ul id="menu-meu-pedido-1" class="menu"><li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-59"><a href="http://saborbrasilrestaurante.com.br/">INÍCIO</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-65"><a href="http://saborbrasilrestaurante.com.br/?page_id=64">CARDÁPIO DO DIA</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-60"> <a href="wpsaborbrasil/testeJavaSLogin.php">FAZER PEDIDO</a>FAZER PEDIDO</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><a href="http://saborbrasilrestaurante.com.br/?page_id=70">CADASTRE-SE AQUI</a></li>
+</ul></div>      
+        </div>
+      <div class="right">
+      <p>Sabor Brasil 2017 | All Rights Reserved.<br />
+      IsyFood.com <a href=http://isyfood.com/ target='_blank'>SpheraSystems</a></p>
+        </div>
+        <div class="clear"></div>
+    </div>
+</footer>
+</div>
+<script type='text/javascript' src='http://saborbrasilrestaurante.com.br/wp-includes/js/wp-embed.min.js?ver=4.7.1'></script>
+</body>
+</html>
